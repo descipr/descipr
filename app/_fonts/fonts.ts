19f9a408f8055b30fa4d5812e3fb7d9d1766c742
descipr <hello@descipr.com>
2025-08-25 15:53:23 +0530

    Replace old app with static build

--- app/_fonts/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Plus_Jakarta_Sans } from "next/font/google";
-
-export const plus_jakarta = Plus_Jakarta_Sans({
-    weight : ['200' , '400' ,'500' ,'600' ,'700'],
-    subsets: ['latin'],
-    variable:'--font--plus-jakarta'
-})
